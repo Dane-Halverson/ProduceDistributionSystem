0a9ac66d45433b3f985af8c4434401c3dca8cb05
Teddy Jeddeloh <jedde020@d.umn.edu>
2022-10-12 16:28:22 -0500

    json files added, json cpp created(unfinished)

---
+++ JSONP.cpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "ProduceInterface.h"
+
+class JSONP {
+private:
+    
+public:
+    JSONP(/* args */);
+    ~JSONP();
+
+};
+
+JSONP::JSONP(/* args */){
+}
+
+JSONP::~JSONP() {
+}
