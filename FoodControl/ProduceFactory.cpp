1d4b49ff4ede266a63929a349ab6b4018ccd1f6a
Marshall Bluhm <bluhm101@d.umn.edu>
2022-10-18 16:00:34 -0500

    Added basic ProduceFactory still need to get it completly working

--- FoodControl/ProduceFactory.cpp
+++ FoodControl/ProduceFactory.cpp
@@ -1,14 +1,40 @@
 #include "ProduceFactory.h"
+#include "Produce/tomatoes.h"
+#include "Produce/apple.h"
+#include "Produce/lettuce.h"
+#include "Produce/carrots.h"
+#include "Produce/onions.h"
+#include "Produce/squash.h"
+
 
 ProduceFactory::ProduceFactory(std::vector<std::string> data){
     int size = data.size();
-    for (int i = 0; i<size; i++){
-        for (int j = 0; j <valid_produce.size(); j++){
-            if (data[i] == valid_produce[j]){
-                std::cout << valid_produce[j] << std::endl;                 
-
+    std::string date = "10/17/2022";
+    int weight = stoi(data[1]);
+    double cost = stod(data[2]);
+    for (int j = 0; j <valid_produce.size(); j++){
+        if (data[0] == valid_produce[j]){
+            std::cout << valid_produce[j] << std::endl;
+            if (valid_produce[j] == "tomatoes"){
+                //Produce *ptr = new tomatoes();
+                tomatoes(weight, date, 1, data[3], cost);
+            }
+            else if (valid_produce[j] == "lettuce"){
+                lettuce(weight, date, 1, data[3], cost);
+            }
+            else if (valid_produce[j] == "carrots"){
+                carrots(weight, date, 1, data[3], cost);
+            }
+            else if (valid_produce[j] == "apple"){
+                apple(weight, date, 1, data[3], cost);
+            }
+            else if (valid_produce[j] == "onions"){
+                onions(weight, date, 1, data[3], cost);
+            }
+            else if (valid_produce[j] == "squash"){
+                squash(weight, date, 1, data[3], cost);
             }
         }
-            
     }
-};
+}
+
--- FoodControl/ProduceFactory.h
+++ FoodControl/ProduceFactory.h
@@ -2,8 +2,6 @@
 #include <vector>
 #include <string>
 #include <iostream>
-// #include "ProduceInterface.h"
-// #include "Produce/tomatoes.h"
 
 /**
  * @author Marshall Bluhm
@@ -18,20 +16,17 @@
 */
 class ProduceFactory {
     public:
-
         /**
-         * @brief Sets the produce given a vector of strings
+         * @brief Sets the produce given a vector of strings of given data {type, weight, costPerPound, farm}
          * @param std::vector<std::string>
         */
         ProduceFactory(std::vector<std::string> data);
-        
         /**
          * @brief default deconstructor
         */
         ~ProduceFactory() = default;
 
     private:
-
         std::vector<std::string> valid_produce = {"apples", "carrots", "tomatoes", "squash", "lettuce", "onions"};
 
 
