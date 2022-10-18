1a52737aed4975756de8b058a9aa5d6a29c0734c
Marshall Bluhm <bluhm101@d.umn.edu>
2022-10-17 21:21:41 -0500

    removed and reorginized files

--- Produce/ProduceFactory.cpp
+++
@@ -1,8 +0,0 @@
-#include "ProduceFactory.h"
-
-ProduceFactory::ProduceFactory(std::vector<std::string> data){
-    int size = data.size();
-    for (int i = 0; i<size; i++){
-        std::cout << data[i] << std::endl;
-    };
-}
