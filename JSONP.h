1493c0aadc405494012f27528841b69d84668a23
Dane Halverson <halve564@d.umn.edu>
2022-10-18 14:36:25 -0500

    wrote function declarations for json parser

--- JSONP.h
+++ JSONP.h
@@ -9,8 +9,24 @@
 class JSONP {
 private:
     std::string filename;
+    void parse();
+    nlohmann::json Produce;
+    nlohmann::json Schools;
 public:
     JSONP(const std::string fn) : filename(fn) {}
     ~JSONP() = default;
-    void parce();
+    std::string getDate();
+    std::string getProducecount();
+    int getProduce(int i);
+    int getProduceInfo(int i, std::string data);
+    bool produceHasDate();
+
+    int getSchoolCount();
+    std::string getSchool(int i);
+
+    //may wont seperate functions for getting each datapiece ie. days since pick, date, weight, farm etc.
+    std::string getSchoolInfo(int i, std::string data);
+
+
+    
 };
