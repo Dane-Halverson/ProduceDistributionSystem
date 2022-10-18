6702bd28d262758fcc0be825d479e52dcf2cf692
Marshall Bluhm <bluhm101@d.umn.edu>
2022-10-18 17:07:32 -0500

    Seperated ProduceInterface implementaion and def

--- FoodControl/ProduceInterface.h
+++ FoodControl/ProduceInterface.h
@@ -57,30 +57,3 @@ class Produce {
     std::string getFarm() const;
 
 };
-
-Produce::Produce(int n, std::string date, int daysSincePick, std::string nameOfFarm, double price) : weight(n), farm(nameOfFarm)
-{
-    pricePerPound = price;
-    experation.setDate(date);
-    experation.addDays(life - daysSincePick);
-}
-
-int Produce::getExperInt() {
-    return experation.getDaysSinceZero();
-}
-
-double Produce::getPricePerPound() const {
-    return pricePerPound;
-}
-
-int Produce::getWeight() const {
-    return weight;
-}
-
-void Produce::setWeight(int amount) {
-    weight = amount;
-}
-
-std::string Produce::getFarm() const {
-    return farm;
-}
