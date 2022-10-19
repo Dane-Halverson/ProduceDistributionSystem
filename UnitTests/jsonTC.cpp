eccb7f3937bc183e08857a95b20cf4cdf71a4ae4
Teddy Jeddeloh <jedde020@d.umn.edu>
2022-10-19 11:24:10 -0500

    added several more tests to json

--- UnitTests/jsonTC.cpp
+++ UnitTests/jsonTC.cpp
@@ -7,6 +7,10 @@ TEST_CASE("JSONP basic operations") {
     JSONP test("../testJSONP.json", "../testJSONS.json");
     REQUIRE(test.getDate() == "Sept 18 2021");
     REQUIRE(test.produceHasExperation(0) == false);
-
-
+    REQUIRE(test.getProduceCount() == 3);
+    REQUIRE(test.getDaysSincePicked() == 1);
+    REQUIRE(test.getProduceWeight(2) == 12);
+    REQUIRE(test.getProduceType(1) == "tomatoes");
+    REQUIRE(test.getCPP(3) == 0.5);
+    REQUIRE(test.getProduceExperation(1) == 1);
 }
