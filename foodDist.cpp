79af95c0c44b2301c6a56b75a743b240a88c654d
Dane Halverson <halve564.d.umn.edu>
2022-10-25 02:10:22 -0500

    fixed filepaths for windows vs

--- foodDist.cpp
+++ foodDist.cpp
@@ -6,10 +6,12 @@
 int main(int, char**)
 {
   FoodDist_GUI foodDistGUI;
+  std::string pf = "C:/Users/USER/source/repos/Food-Distribution-System-new/testJSONP.json";
+  std::string sf = "C:/Users/USER/source/repos/Food-Distribution-System-new/testJSONS.json";
 
   // Create the Data Source and provide to the GUI
   std::shared_ptr<FoodDist_DataSource> fdistSysPtr =
-	  std::make_shared<FoodDistributor>("../testJSONP.json", "../testJSONS.json");
+	  std::make_shared<FoodDistributor>(pf,sf);
   foodDistGUI.assignFoodDistSystem( fdistSysPtr );
 
   foodDistGUI.mainLoop();
