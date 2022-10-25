fb9a2ffdeec33c1d682f7e76b346f4ae29a1a4fe
Dane Halverson <halve564@d.umn.edu>
2022-10-25 16:27:17 -0500

    fixed produce factory to make proper objects

--- ProduceDistribution/lib/FoodControl/ProduceFactory.cpp
+++ ProduceDistribution/lib/FoodControl/ProduceFactory.cpp
@@ -10,22 +10,22 @@
 std::shared_ptr<Produce> ProduceFactory::makeProduce(std::string type, int n, std::string date, int daysSincePick, std::string nameOfFarm, double price){
     std::shared_ptr<Produce> NewProduce;
     if (type == "tomatoes"){
-        NewProduce = std::make_shared<tomatoes>((n, date, daysSincePick, nameOfFarm, price));
+        NewProduce = std::make_shared<tomatoes>(n, date, daysSincePick, nameOfFarm, price);
     }
     else if (type == "lettuce"){
-        NewProduce = std::make_shared<Produce>(lettuce(n, date, daysSincePick, nameOfFarm, price));
+        NewProduce = std::make_shared<lettuce>(n, date, daysSincePick, nameOfFarm, price);
     }
     else if (type == "carrots"){
-        NewProduce = std::make_shared<Produce>(carrots(n, date, daysSincePick, nameOfFarm, price));
+        NewProduce = std::make_shared<carrots>(n, date, daysSincePick, nameOfFarm, price);
     }
     else if (type == "apples"){
-        NewProduce = std::make_shared<Produce>(apple(n, date, daysSincePick, nameOfFarm, price));
+        NewProduce = std::make_shared<apple>(n, date, daysSincePick, nameOfFarm, price);
     }
     else if (type == "onions"){
-        NewProduce = std::make_shared<Produce>(onions(n, date, daysSincePick, nameOfFarm, price));
+        NewProduce = std::make_shared<onions>(n, date, daysSincePick, nameOfFarm, price);
     }
     else if (type == "squash"){
-        NewProduce = std::make_shared<Produce>(squash(n, date, daysSincePick, nameOfFarm, price));
+        NewProduce = std::make_shared<squash>(n, date, daysSincePick, nameOfFarm, price);
     }
     return NewProduce; 
 };
